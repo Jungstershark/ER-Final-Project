66ac2ec52bfde413029f001e71e07acb555429b5
Hung Luong <hung.lv.2601@gmail.com>
2025-12-10 18:44:41 +0800

    delete using Unity.VisualScripting;

--- Assets/Scripts/SO/GameEvent.cs
+++ Assets/Scripts/SO/GameEvent.cs
@@ -1,28 +1,40 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-
 
 public class GameEvent<T> : ScriptableObject
 {
     private readonly List<GameEventListener<T>> eventListeners =
         new List<GameEventListener<T>>();
 
+    /// <summary>
+    /// Raises this event and notifies all listeners.
+    /// </summary>
     public void Raise(T data)
     {
-        for (int i = eventListeners.Count - 1; i >= 0; i--)
-            eventListeners[i].OnEventRaised(data);
+        var listenersSnapshot = eventListeners.ToArray();
+
+        for (int i = listenersSnapshot.Length - 1; i >= 0; i--)
+        {
+            listenersSnapshot[i].OnEventRaised(data);
+        }
     }
 
+    /// <summary>
+    /// Registers a listener to be notified when this event is raised.
+    /// </summary>
     public void RegisterListener(GameEventListener<T> listener)
     {
+        if (listener == null) return;
         if (!eventListeners.Contains(listener))
             eventListeners.Add(listener);
     }
 
+    /// <summary>
+    /// Unregisters a listener.
+    /// </summary>
     public void UnregisterListener(GameEventListener<T> listener)
     {
-        if (eventListeners.Contains(listener))
-            eventListeners.Remove(listener);
+        if (listener == null) return;
+        eventListeners.Remove(listener);
     }
 }
