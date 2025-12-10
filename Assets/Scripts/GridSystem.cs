fdcedfe6afb5c946f7ed666c1d5c34dcbd13e541
FooChuanTian <foochuantian@gmail.com>
2025-12-11 00:41:40 +0800

    Update GridSystem.cs

--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -100,14 +100,20 @@ public class GridSystem : MonoBehaviour
         int currentCombiLength = this.currentCombi.Count();
         Debug.Log("CurrentCombiLength: " + currentCombiLength.ToString());
         Debug.Log("Registered Click on " + row.ToString() +  ' ' + col.ToString());
-        if (!(this.currentCombi[currentCombiLength-1].Item1 == row && this.currentCombi[currentCombiLength-1].Item2 == col)) 
+        if (this.currentCombi.Count > 0) {
+            if (!(this.currentCombi[currentCombiLength-1].Item1 == row && this.currentCombi[currentCombiLength-1].Item2 == col)) 
+            {
+                Debug.Log("ABCD");
+                this.currentCombi.Add((row, col));
+                Debug.Log("Added to currentCombi");
+                PrintCurrentCombi();
+
+
+            }
+        }
+        else
         {
-            Debug.Log("ABCD");
-            this.currentCombi.Add((row, col));
-            Debug.Log("Added to currentCombi");
-            PrintCurrentCombi();
-
-
+           this.currentCombi.Add((row, col)); 
         }
         Debug.Log("EFGH");
         PrintCurrentCombi();
