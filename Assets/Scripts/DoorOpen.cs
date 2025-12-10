b36123b4e771e97ccb5166e881c140f77539d6c8
DougTCH <douglastan037@gmail.com>
2025-12-11 02:59:46 +0800

    Door Fixed

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 
 public class DoorOpen : MonoBehaviour
 {
-    public UnityEvent SimpleGameEvent;
+
     public GameObject doorObj;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
