ea89eeeedfc63c0ff4f444e97e7953c5025b71c2
FooChuanTian <foochuantian@gmail.com>
2025-12-10 15:33:49 +0800

    WIP linking SpellTableOrbs to GridSystem

--- Assets/Scripts/SpellTableOrbs.cs
+++ Assets/Scripts/SpellTableOrbs.cs
@@ -1,17 +1,26 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class SpellTableOrbs : MonoBehaviour
 {
     public GameObject orbObject;
+    public int row;
+    public int col;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OrbSelected()
     {
+        Debug.Log(orbObject.name+" selected.");
         orbObject.GetComponent<Renderer>().material.color = Color.red;
+        GridSystem.Instance.RegisterOrbClick(this.row, this.col);
     }
 
     public void OrbDeselected()
     {
+        Debug.Log(orbObject.name+" deselected.");
         orbObject.GetComponent<Renderer>().material.color = Color.white;
     }
 
